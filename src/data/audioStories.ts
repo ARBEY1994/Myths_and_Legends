4c10c0de43f3934df8b1b05eaf982f0fd6c50fa1
heiner <arbey221@gmail.com>
2025-05-31 20:16:32 -0500

    feat: add audio stories data and mp3 files for myths and legends collection

--- src/data/audioStories.ts
+++ src/data/audioStories.ts
@@ -31,5 +31,21 @@ export const audioStories: AudioStory[] = [
     audioUrl: "/audio/LaSubidadelMuerto.mp3",
     imageUrl: "https://res.cloudinary.com/dwwdj1lkw/image/upload/v1748727138/mitos%20y%20leyendas/AudioHistorias/laSubidaDelMuerto_ohwjp4.png",
     duration: "02:42"
+  },
+  {
+    id: 4,
+    title: "La Mujer De La Mucura",
+    description: "La Historia de una mujer que lleva una gran carga en sus hombros y que trasciende de este mundo.",
+    audioUrl: "/audio/laMucura.mp3",
+    imageUrl: "https://res.cloudinary.com/dwwdj1lkw/image/upload/v1748740477/mitos%20y%20leyendas/AudioHistorias/Gemini_Generated_Image_cmi1micmi1micmi1_uaurql.png",
+    duration: "02:27"
+  },
+  {
+    id: 5,
+    title: "Mi Esposa Es Un Demonio",
+    description: "La historia  de una mujer que no es humana, esposa de un hombre que la ama y que le causa miedo.",
+    audioUrl: "/audio/esposaSiniestra.mp3",
+    imageUrl: "https://res.cloudinary.com/dwwdj1lkw/image/upload/v1748740448/mitos%20y%20leyendas/AudioHistorias/Gemini_Generated_Image_d6tui0d6tui0d6tu_rsyxga.png",
+    duration: "04:51"
   }
 ];
